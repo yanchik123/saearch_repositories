07f1ff422442c9753fdbb6edb0eea672ac021cfc
Yan Front-End <uan.solodovnikov@bk.ru>
2017-03-31 16:01:13 +0300

    new

--- app/controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.main = function(req, res){
-    var path = require('path');
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-};
-
-module.exports.videos = require('./videos');
